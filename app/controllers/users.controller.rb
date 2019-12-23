bb24780682fbe99f5efa75c22c6fc4baacc40511
akinori0714 <akinori_ichimura@yahoo.co.jp>
2019-12-23 16:20:21 +0900

    修正

--- app/controllers/users.controller.rb
+++ app/controllers/users.controller.rb
@@ -3,10 +3,6 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def new
-    @user = User.new
-  end
-
   def update
     if current_user.update(user_params)
       redirect_to root_path
